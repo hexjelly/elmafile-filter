1af2cf924fe985d4974a3280de82251287bd850d
Roger Andersen <hexjelly@hexjelly.com>
2016-04-14 21:07:12 +0200

    Add object tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -43,22 +43,70 @@ mod tests {
         assert_eq!(level.sky, String::from("sky"));
         assert_eq!(level.polygons.len(), 2);
         assert_eq!(level.polygons, vec![lev::Polygon {
-                                                grass: false, vertices: vec![
-                                                    Position { x: -23.993693053024586, y: -3.135779367971911 },
-                                                    Position { x: -15.989070625361132, y: -3.135779367971911 },
-                                                    Position { x: -15.989070625361132, y: 1.995755366905195 },
-                                                    Position { x: -24f64, y: 2f64 }
+                                            grass: false, vertices: vec![
+                                                Position { x: -23.993693053024586, y: -3.135779367971911 },
+                                                Position { x: -15.989070625361132, y: -3.135779367971911 },
+                                                Position { x: -15.989070625361132, y: 1.995755366905195 },
+                                                Position { x: -24f64, y: 2f64 }
                                             ]},
-                                            lev::Polygon {
-                                                grass: true, vertices: vec![
-                                                    Position { x: -23.83645939819548, y: 2.310222676563402 },
-                                                    Position { x: -17.60428907951465, y: 2.2816347393217473 },
-                                                    Position { x: -17.53281923641051, y: 1.8956975865594021 },
-                                                    Position { x: -23.96510511578293, y: 1.924285523801057 }
+                                        lev::Polygon {
+                                            grass: true, vertices: vec![
+                                                Position { x: -23.83645939819548, y: 2.310222676563402 },
+                                                Position { x: -17.60428907951465, y: 2.2816347393217473 },
+                                                Position { x: -17.53281923641051, y: 1.8956975865594021 },
+                                                Position { x: -23.96510511578293, y: 1.924285523801057 }
                                             ]}
                                         ]);
-        // TODO: proper object tests
         assert_eq!(level.objects.len(), 8);
+        assert_eq!(level.objects, vec![lev::Object {
+                                            position: Position { x: -23.221818747499896, y: -1.3204453531268072 },
+                                            object_type: lev::ObjectType::Killer,
+                                            gravity: 0,
+                                            animation: 1
+                                        },
+                                        lev::Object {
+                                            position: Position { x: -20.37252715482359, y: -0.3124543521844827 },
+                                            object_type: lev::ObjectType::Apple,
+                                            gravity: 0,
+                                            animation: 9
+                                        },
+                                        lev::Object {
+                                            position: Position { x: -20.3914786548306, y: 0.5277288147929609 },
+                                            object_type: lev::ObjectType::Apple,
+                                            gravity: 1,
+                                            animation: 1
+                                        },
+                                        lev::Object {
+                                            position: Position { x: -19.526026821177144, y: 0.36348248139887396 },
+                                            object_type: lev::ObjectType::Apple,
+                                            gravity: 4,
+                                            animation: 5
+                                        },
+                                        lev::Object {
+                                            position: Position { x: -21.269564821822065, y: 0.38243398140588436 },
+                                            object_type: lev::ObjectType::Apple,
+                                            gravity: 3,
+                                            animation: 1
+                                        },
+                                        lev::Object {
+                                            position: Position { x: -19.55761265452216, y: -0.4387976855645497 },
+                                            object_type: lev::ObjectType::Apple,
+                                            gravity: 1,
+                                            animation: 1
+                                        },
+                                        lev::Object {
+                                            position: Position { x: -20.075620321380434, y: -1.2473950191969765 },
+                                            object_type: lev::ObjectType::Exit,
+                                            gravity: 0,
+                                            animation: 1
+                                        },
+                                        lev::Object {
+                                            position: Position { x: -22.94993115577695, y: 1.5068896484884773 },
+                                            object_type: lev::ObjectType::Player,
+                                            gravity: 0,
+                                            animation: 1
+                                        }]);
+
         // TODO: proper pictures tests
         assert_eq!(level.pictures.len(), 2);
         // TODO: test top10 list
