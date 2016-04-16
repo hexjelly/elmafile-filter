1a0881ea1454840af6f2d3cd3fa675d840772bc3
Roger Andersen <hexjelly@hexjelly.com>
2016-04-16 17:29:45 +0200

    Add some comments to test file
    Just to structure it out a bit more, and explain the tests briefly.

--- tests/lib.rs
+++ tests/lib.rs
@@ -17,7 +17,9 @@ mod tests {
     }*/
 
     #[test]
-    fn test_lev_default_values () {
+    // Probably redundant, but maybe some new fields are added in the future.
+    // Doesn't hurt or impact anything.
+    fn level_default_values () {
         let level = lev::Level::new();
         assert_eq!(level.version, lev::Version::Elma);
         assert_eq!(level.link, 0);
@@ -29,7 +31,7 @@ mod tests {
     }
 
     #[test]
-    fn test_lev_load_level () {
+    fn load_level_1 () {
         let level = lev::Level::load_level("tests/test.lev");
         assert_eq!(level.version, lev::Version::Elma);
         assert_eq!(level.link, 1524269776);
@@ -41,22 +43,25 @@ mod tests {
         assert_eq!(level.lgr, String::from("default"));
         assert_eq!(level.ground, String::from("ground"));
         assert_eq!(level.sky, String::from("sky"));
+
+        // Polygon tests.
         assert_eq!(level.polygons.len(), 2);
         assert_eq!(level.polygons, vec![lev::Polygon {
                                             grass: false, vertices: vec![
                                                 Position { x: -23.993693053024586, y: -3.135779367971911 },
                                                 Position { x: -15.989070625361132, y: -3.135779367971911 },
                                                 Position { x: -15.989070625361132, y: 1.995755366905195 },
-                                                Position { x: -24f64, y: 2f64 }
-                                            ]},
+                                                Position { x: -24f64, y: 2f64 }]
+                                        },
                                         lev::Polygon {
                                             grass: true, vertices: vec![
                                                 Position { x: -23.83645939819548, y: 2.310222676563402 },
                                                 Position { x: -17.60428907951465, y: 2.2816347393217473 },
                                                 Position { x: -17.53281923641051, y: 1.8956975865594021 },
-                                                Position { x: -23.96510511578293, y: 1.924285523801057 }
-                                            ]}
-                                        ]);
+                                                Position { x: -23.96510511578293, y: 1.924285523801057 }]
+                                        }]);
+
+        // Object tests.
         assert_eq!(level.objects.len(), 8);
         assert_eq!(level.objects, vec![lev::Object {
                                             position: Position { x: -23.221818747499896, y: -1.3204453531268072 },
@@ -106,6 +111,8 @@ mod tests {
                                             gravity: 0,
                                             animation: 1
                                         }]);
+
+        // Picture tests.
         assert_eq!(level.pictures.len(), 2);
         assert_eq!(level.pictures, vec![lev::Picture {
                                             name: String::from("barrel"),
@@ -123,11 +130,14 @@ mod tests {
                                             distance: 750,
                                             clip: 2
                                         }]);
+
         // TODO: test top10 list
     }
 
     #[test]
-    fn test_rec_default_values () {
+    // Probably redundant, but maybe some new fields are added in the future.
+    // Doesn't hurt or impact anything.
+    fn rec_default_values () {
         let rec = rec::Rec::new();
         assert_eq!(rec.frame_count, 0);
         assert_eq!(rec.multi, false);
