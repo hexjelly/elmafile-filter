ff091e971dfd713f12aee5291d4d10c359520294
Roger Andersen <hexjelly@hexjelly.com>
2018-05-12 16:55:45 +0200

    fix: ignore random bytes, pretend unused part of string, hope it keeps working

--- tests/lgr.rs
+++ tests/lgr.rs
@@ -1,11 +1,10 @@
 extern crate elma;
 
 use elma::lgr::*;
+use std::env;
 use std::fs;
 
 #[test]
-// Probably redundant, but maybe some new fields are added in the future. I don't know.
-// Doesn't hurt or impact anything.
 fn correctly_loads_lgr_1() {
     let lgr = LGR::load("tests/assets/lgr/Default.lgr");
     assert!(lgr.is_ok());
@@ -15,4 +14,20 @@ fn correctly_loads_lgr_1() {
 fn correctly_loads_lgr_2() {
     let lgr = LGR::load("tests/assets/lgr/Across.lgr");
     assert!(lgr.is_ok());
+}
+
+#[test]
+fn correctly_loads_saves_and_reloads_lgrs() {
+    let mut dir = env::temp_dir();
+    dir.push("resaved.lgr");
+
+    let orig_default_lgr = LGR::load("tests/assets/lgr/Default.lgr").unwrap();
+    orig_default_lgr.save(&dir).unwrap();
+    let reloaded_default_lgr = LGR::load(&dir).unwrap();
+    assert_eq!(orig_default_lgr, reloaded_default_lgr);
+
+    let orig_across_lgr = LGR::load("tests/assets/lgr/Across.lgr").unwrap();
+    orig_across_lgr.save(&dir).unwrap();
+    let reloaded_across_lgr = LGR::load(&dir).unwrap();
+    assert_eq!(orig_across_lgr, reloaded_across_lgr);
 }
