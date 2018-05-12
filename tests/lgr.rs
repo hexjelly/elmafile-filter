820e428db525e0247d2a014e94c53a2ce93aabed
Roger Andersen <hexjelly@hexjelly.com>
2018-05-12 13:52:41 +0200

    add: Across.lgr for second test

--- tests/lgr.rs
+++ tests/lgr.rs
@@ -1,15 +1,18 @@
 extern crate elma;
 
 use elma::lgr::*;
-use elma::{Clip, ElmaError};
-use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 
 #[test]
 // Probably redundant, but maybe some new fields are added in the future. I don't know.
 // Doesn't hurt or impact anything.
-fn correctly_loads_lgr() {
+fn correctly_loads_lgr_1() {
     let lgr = LGR::load("tests/assets/lgr/Default.lgr");
+    assert!(lgr.is_ok());
+}
+
+#[test]
+fn correctly_loads_lgr_2() {
+    let lgr = LGR::load("tests/assets/lgr/Across.lgr");
     assert!(lgr.is_ok());
 }
